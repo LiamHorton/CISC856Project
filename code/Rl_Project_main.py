c7b5f34c74f07be56f82aa6a6f75cf036b823067
JasonDHarris <55200321+JasonDHarris@users.noreply.github.com>
2022-04-16 17:43:24 -0400

    debugging for PC

--- code/Rl_Project_main.py
+++ code/Rl_Project_main.py
@@ -1,7 +1,12 @@
+#%%
+
+# IMPORT LIBRARIES
+
 import glob
 import os
 from pickle import TRUE
 from queue import Queue
+from tkinter import Y
 import matplotlib.pyplot as plt
 import sys
 import cv2
@@ -18,7 +23,7 @@ except IndexError:
 
 
 import carla
-from queue import Queue
+#from queue import Queue
 from queue import Empty
 from PIL import Image
 import numpy as np
@@ -27,77 +32,123 @@ import Carla_funcs as cf
 import gcg
 
 
+
+
 # %%
 # RL variables
 H = 8
 K = 5
-action_space = np.array([-0.05, 0, 0.05])
+action_space = np.array([-0.2, 0, 0.2])
+vehicle_speed = 10
+
 
 # %%
 # Initialize Network
 
-model = gcg.computation_graph()
+model = gcg.computation_graph(H)
 
 # %%
 #  Carla Setup
-image_queue = Queue()
-collision_queue = Queue()
+# image_queue = Queue()
+# collision_queue = Queue()
 
-client, world, vehicle, camera, collision, orig_settings = cf.setup(collision_queue, image_queue, time_step = 0.1, img_x = 300, img_y = 400)
+client, world, vehicle, camera, collision, orig_settings, image_queue, collision_queue = cf.setup(time_step = 1, img_x = 128, img_y = 72, speed=vehicle_speed)
+    
 
 # %%
+
 # Begin loop
-big_loop_counter = 5
+big_loop_counter = 2
 step_max = 300000
 img_stack = None
+
+cum_steps = 0
+cum_steps_per_ep = []
+steps_per_ep = []
+
 for i in range(big_loop_counter):
     # Initialize datasets
     dataset_I = []
     dataset_a = []
     y_labels = []
-    y_buffer = np.empty([1,H])
-    cum_steps = 0
-    cum_steps_per_ep = []
-    steps_per_ep = []
+    y_buffer = np.zeros(H)
+
     # Sim loop
     episode_done = False
     # get an initial image
+    
     world.get_snapshot().frame
     img = image_queue.get(True, 1.0)
+    
     for step in range(step_max):
+    #for step in range(9):
         img_stack = cf.preprocess_img(img, img_stack)
         rwd_list = []
         action_sets = []
+    
         for k in range(K):
             action_input = generate_actions(action_space, H)
             y_hats = gcg.run(model, img_stack, action_input)
             rwd = reward_function(y_hats)
             rwd_list.append(rwd)
             action_sets.append(action_input)
+    
         best_action_set = best_actions(action_sets, rwd_list)
-        img, collided = cf.take_action(world, vehicle, image_queue, collision_queue, best_actions[0])
+        img, collided = cf.take_action(world, vehicle, image_queue, collision_queue, best_action_set[0])
         dataset_I.append(img_stack)
         dataset_a.append(best_action_set)
-        if step<H:
-            y_buffer[step] = collided
-        elif step == H:
-            y_labels.append(y_buffer)
-        else:
-            y_buffer[0:-2] = y_buffer[1:-1]
-            y_buffer[-1] = collided
-            y_labels.append(y_buffer)
+        
         if collided == 1:
-            break
-    for i in range(H-1):
-        y_buffer[0:-2] = y_buffer[1:-1]
-        y_buffer[-1] = 1
-        y_labels.append(y_buffer)
+            if step+1 >= H:
+                
+                for i in range(H):
+                    y_buffer[H-1-i] = 1
+                    y_labels.append(y_buffer.copy())
+                break
+
+            else:
+                for i in range(step):
+                    y_buffer[H-i-step:] = 1
+                    y_labels.append(y_buffer.copy())
+                break
+        elif  step+1 >= H:
+            y_labels.append(y_buffer.copy())
+
     cum_steps += step
-    cum_steps_per_ep.append(step)
+    cum_steps_per_ep.append(cum_steps)
     steps_per_ep.append(step)
+
 
     gcg.train(model, dataset_I, dataset_a, y_labels)
 
+    cf.close(world, camera, collision, vehicle, orig_settings)
+    client, world, vehicle, camera, collision, orig_settings, image_queue, collision_queue = cf.setup(time_step = 1, img_x = 128, img_y = 72, speed=vehicle_speed)
+
+# %%
+model.save('../models/model.tf')
+
 # %%
 # End and exit
-carla.close(world, camera, collision, vehicle, orig_settings)
+cf.close(world, camera, collision, vehicle, orig_settings)
+# %%
+
+
+fig1 = plt.figure()
+plt.plot(range(big_loop_counter), cum_steps_per_ep)
+plt.xlabel('Episodes')
+plt.ylabel('Cumulative Moves')
+plt.title('Figure 1')
+plt.savefig('../images/figure1.png')
+
+
+fig2 = plt.figure()
+plt.plot(range(big_loop_counter), steps_per_ep)
+plt.xlabel('Episodes')
+plt.ylabel('Moves')
+plt.title('Figure 2')
+plt.savefig('../images/figure2.png')
+plt.show()
+
+
+
+# %%
--- code/babysteps.py
+++ code/babysteps.py
@@ -5,6 +5,7 @@ from queue import Queue
 import matplotlib.pyplot as plt
 import sys
 import cv2
+import time
 
 from zmq import QUEUE
 
@@ -42,10 +43,10 @@ def run_simulation(client):
         original_settings = world.get_settings()
 
         # Configure the world
-        settings = world.get_settings()
-        settings.synchronous_mode = True #make the server wait for the client
-        settings.fixed_delta_seconds = time_step
-        world.apply_settings(settings)
+        # settings = world.get_settings()
+        # settings.synchronous_mode = True #make the server wait for the client
+        # settings.fixed_delta_seconds = time_step
+        # world.apply_settings(settings)
       
         vehicle = None
         camera = None
@@ -61,74 +62,92 @@ def run_simulation(client):
         camera_bp.set_attribute("image_size_y", '72')
         # Consider adding noise and and blurring with enable_postprocess_effects
         
-        
+
+
 
         # Spawn our actors
-        vehicle = world.spawn_actor(blueprint=vehicle_bp, transform=world.get_map().get_spawn_points()[0])
+        vehicle = world.spawn_actor(blueprint=vehicle_bp, transform=world.get_map().get_spawn_points()[130])
         camera = world.spawn_actor(blueprint=camera_bp, transform=carla.Transform(carla.Location(x=3.0, z=1.2)), attach_to=vehicle)
         collision = world.spawn_actor(blueprint=collision_bp, transform=carla.Transform(), attach_to=vehicle)
 
-        img_stack = []
-        image_queue = Queue()
-        camera.listen(lambda data: sensor_callback(data, image_queue))
-        collision_queue = Queue()
-        collision.listen(lambda data: sensor_callback(data, collision_queue))
+        physics_control = vehicle.get_physics_control()
+        speed_v = carla.Vector3D(x=10.0, y=0.0, z=0.0)
+        vehicle.enable_constant_velocity(speed_v)
+        vehicle.apply_control(carla.VehicleControl(steer=0))
+        time.sleep(3)
+        vehicle.apply_control(carla.VehicleControl(steer=-0.1))
+        time.sleep(3)
+        vehicle.apply_control(carla.VehicleControl(steer=0.0))
+        time.sleep(3)
+        vehicle.apply_control(carla.VehicleControl(steer=0.1))
+        time.sleep(3)
+        vehicle.apply_control(carla.VehicleControl(steer=0.0))
+        time.sleep(3)
+        print(vehicle.get_velocity())
 
-        steering = 0
-        steering_list=[0, 0.05, -0.05]
-        #for step in range(int(sim_time/time_step)):
-        for step in range(4):
-            if (step+1)%25==0 and step > 100:
-                # turn = not(turn)
-                # if turn:
-                #     right_turn = not(right_turn)
-                vehicle.apply_control(carla.VehicleControl(throttle=0.2, steer=steering))
-                steering = min(max(-0.10, steering + np.random.choice(steering_list, p=[1/3,1/3, 1-2/3])), 0.10)
-            elif step <= 100:
-                vehicle.apply_control(carla.VehicleControl(throttle=0.2, steer=steering))
+
+        # img_stack = []
+        # image_queue = Queue()
+        # camera.listen(lambda data: sensor_callback(data, image_queue))
+        # collision_queue = Queue()
+        # collision.listen(lambda data: sensor_callback(data, collision_queue))
+
+        # steering = 0
+        # steering_list=[0, 0.05, -0.05]
+        # #for step in range(int(sim_time/time_step)):
+        # for step in range(4):
+        #     if (step+1)%25==0 and step > 100:
+        #         # turn = not(turn)
+        #         # if turn:
+        #         #     right_turn = not(right_turn)
+        #         vehicle.apply_control(carla.VehicleControl(throttle=0.1, steer=steering))
+        #         steering = min(max(-0.10, steering + np.random.choice(steering_list, p=[1/3,1/3, 1-2/3])), 0.10)
+        #     elif step <= 100:
+        #         vehicle.apply_control(carla.VehicleControl(throttle=0.2, steer=steering))
 
             
-            # if turn and right_turn:
-            #     vehicle.apply_control(carla.VehicleControl(throttle=0.2, steer=0.5))
-            # elif turn:
-            #     vehicle.apply_control(carla.VehicleControl(throttle=0.2, steer=-0.5))
-            # else:
-            #     vehicle.apply_control(carla.VehicleControl(throttle=0.5, steer=0.0))
+        #     # if turn and right_turn:
+        #     #     vehicle.apply_control(carla.VehicleControl(throttle=0.2, steer=0.5))
+        #     # elif turn:
+        #     #     vehicle.apply_control(carla.VehicleControl(throttle=0.2, steer=-0.5))
+        #     # else:
+        #     #     vehicle.apply_control(carla.VehicleControl(throttle=0.5, steer=0.0))
             
 
-            world.tick()
-            world_frame = world.get_snapshot().frame
+        #     world.tick()
+        #     world_frame = world.get_snapshot().frame
 
         
-            image_data = image_queue.get(True, 1.0)
-            try:
-                collision_data = collision_queue.get_nowait()
-                collision_value = 1
-            except Empty:
-                collision_value = 0
+        #     image_data = image_queue.get(True, 1.0)
+        #     try:
+        #         collision_data = collision_queue.get_nowait()
+        #         collision_value = 1
+        #     except Empty:
+        #         collision_value = 0
 
         
-            #output information to the screen
-            sys.stdout.write("\r(%d/%d) Simulation: %d Camera: %d " %
-                (step+1, sim_time/time_step, world_frame, image_data.frame) + ' ' + str(collision_value) + '    ')
-            sys.stdout.flush()
+        #     #output information to the screen
+        #     sys.stdout.write("\r(%d/%d) Simulation: %d Camera: %d " %
+        #         (step+1, sim_time/time_step, world_frame, image_data.frame) + ' ' + str(collision_value) + '    ')
+        #     sys.stdout.flush()
 
-            img_array = np.copy(np.frombuffer(image_data.raw_data, dtype=np.dtype("uint8")))
-            img_array = np.reshape(img_array, (image_data.height, image_data.width, 4))
-            gray_img = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+        #     img_array = np.copy(np.frombuffer(image_data.raw_data, dtype=np.dtype("uint8")))
+        #     img_array = np.reshape(img_array, (image_data.height, image_data.width, 4))
+        #     gray_img = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
 
-            if step == 0:
-                img_stack = ((gray_img),)*4
-                img_stack = np.dstack(img_stack)
-            else:
-                img_stack = np.dstack((img_stack[:,:,1:], gray_img))
+        #     if step == 0:
+        #         img_stack = ((gray_img),)*4
+        #         img_stack = np.dstack(img_stack)
+        #     else:
+        #         img_stack = np.dstack((img_stack[:,:,1:], gray_img))
 
-            cv2.imshow('Gray image', gray_img)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+        #     # cv2.imshow('Gray image', gray_img)
+        #     # cv2.waitKey(0)
+        #     # cv2.destroyAllWindows()
             
-            cv2.imwrite("../output_data/%08d.png" % image_data.frame, gray_img)
+        #     # cv2.imwrite("../output_data/%08d.png" % image_data.frame, gray_img)
 
+        #     np.save('../output_data/test.npy', img_stack)
 
       
     
