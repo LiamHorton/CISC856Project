5fb79437eeae2f7d57c64a672758dc75dda52986
JasonDHarris <55200321+JasonDHarris@users.noreply.github.com>
2022-04-16 19:29:23 -0400

    nda

--- code/Rl_Project_main.py
+++ code/Rl_Project_main.py
@@ -57,7 +57,7 @@ client, world, vehicle, camera, collision, orig_settings, image_queue, collision
 # %%
 
 # Begin loop
-big_loop_counter = 50
+big_loop_counter = 10
 step_max = 300000
 img_stack = None
 
