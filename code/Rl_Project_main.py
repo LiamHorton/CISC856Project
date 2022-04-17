3541032ffb1e2e227d1647177bbb7c01475a3567
JasonDHarris <55200321+JasonDHarris@users.noreply.github.com>
2022-04-17 12:03:13 -0400

    Update Rl_Project_main.py

--- code/Rl_Project_main.py
+++ code/Rl_Project_main.py
@@ -60,7 +60,7 @@ client, world, vehicle, camera, collision, orig_settings, image_queue, collision
 
 # Begin loop
 big_loop_counter = 4000
-step_max = (10 * 60) / delta_t 
+step_max = int((10 * 60) / delta_t)
 img_stack = None
 
 cum_steps = 0
