4ee67f3430c25326b423e3a4a9806924f17f546f
MRCooper <77289850+MRCoope@users.noreply.github.com>
2022-04-17 17:22:38 -0400

    flags and no orig settings

--- code/Carla_funcs.py
+++ code/Carla_funcs.py
@@ -28,6 +28,15 @@ def sensor_callback(data, queue):
     queue.put(data)
 
 def setup(time_step, img_x, img_y, speed = 0.2):
+    setup_queues = 0
+    setup_client_bp_world = 0
+    configure_world = 0
+    get_bp = 0
+    conf_bp = 0
+    spawn_actors = 0
+    tick_snapshot = 0
+    speed_vec = 0
+
     
     try:
 
@@ -40,10 +49,13 @@ def setup(time_step, img_x, img_y, speed = 0.2):
         spawn = random.choice([0,1,7,9,30,50,80])
 
         #setup Queues
+        setup_queues = 1
         image_queue = Queue()
         collision_queue = Queue()
 
+
         # Get the world and its information
+        setup_client_bp_world = 1
         world = client.get_world()
         bp_lib = world.get_blueprint_library()
         orig_settings = world.get_settings()
@@ -51,6 +63,7 @@ def setup(time_step, img_x, img_y, speed = 0.2):
 
 
         # Configure the world
+        configure_world = 1
         settings = world.get_settings()
         settings.synchronous_mode = True  # make the server wait for the client
         settings.fixed_delta_seconds = time_step
@@ -58,17 +71,20 @@ def setup(time_step, img_x, img_y, speed = 0.2):
 
 
         # Get the required blueprints
+        get_bp = 1
         vehicle_bp = bp_lib.filter('cybertruck')[0]
         camera_bp = bp_lib.filter('sensor.camera.rgb')[0]
         collision_bp = world.get_blueprint_library().find('sensor.other.collision')
 
         # # Configure the blueprints
+        conf_bp = 1
         camera_bp.set_attribute("image_size_x", str(img_x))
         camera_bp.set_attribute("image_size_y", str(img_y))
         # Consider adding noise and blurring with enable_postprocess_effects
 
 
         # Spawn our actors
+        spawn_actors = 1
         vehicle = world.spawn_actor(blueprint=vehicle_bp, transform=world.get_map().get_spawn_points()[spawn])
         
         camera = world.spawn_actor(blueprint=camera_bp, transform=carla.Transform(carla.Location(x=3.0, z=1.2)),
@@ -78,16 +94,26 @@ def setup(time_step, img_x, img_y, speed = 0.2):
         camera.listen(lambda data: sensor_callback(data, image_queue))
         collision.listen(lambda data: sensor_callback(data, collision_queue))
 
+        tick_snapshot = 1
         world.tick()
         world.get_snapshot().frame
 
+        speed_vec = 1
         speed_vec = carla.Vector3D(x=speed, y=0.0, z=0.0)
         vehicle.enable_constant_velocity(speed_vec)
 
         return client, world, vehicle, camera, collision, orig_settings, image_queue, collision_queue
     
     except:
-        world.apply_settings(orig_settings)
+        print(f'{str(setup_queues)} = {setup_queues}')
+        print(f'{str(setup_client_bp_world)} = {setup_client_bp_world}')
+        print(f'{str(configure_world)} = {configure_world}')
+        print(f'{str(get_bp)} = {get_bp}')
+        print(f'{str(conf_bp)} = {conf_bp}')
+        print(f'{str(spawn_actors)} = {spawn_actors}')
+        print(f'{str(tick_snapshot)} = {tick_snapshot}')
+        print(f'{str(speed_vec)} = {speed_vec}')
+        # world.apply_settings(orig_settings)
 
         # Destroy the actors in the scene.
         if camera:
@@ -118,7 +144,7 @@ def take_action(world, vehicle, image_queue, past_image, collision_queue, action
 
 def close(world, camera, collision, vehicle, orig_settings):
     # Apply the original settings when exiting.
-    world.apply_settings(orig_settings)
+    # world.apply_settings(orig_settings)
 
     # Destroy the actors in the scene.
     if camera:
