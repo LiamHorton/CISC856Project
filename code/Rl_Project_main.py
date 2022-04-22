54606fd05a29addeafc26de87bc199bdc60b823f
JasonDHarris <55200321+JasonDHarris@users.noreply.github.com>
2022-04-22 11:43:11 -0400

    Project Clean-up for Submissioni

--- code/Rl_Project_main.py
+++ code/Rl_Project_main.py
@@ -1,18 +1,15 @@
 #%%
-
 # IMPORT LIBRARIES
 
 import glob
 import os
-from pickle import TRUE
-from queue import Queue
-from tkinter import Y
 import matplotlib.pyplot as plt
 import sys
-import cv2
-from zmq import QUEUE
-import tensorflow as tf
 import json
+import numpy as np
+from RL_funcs import *
+import Carla_funcs as cf
+import gcg
 
 try:
     sys.path.append(glob.glob('../PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
@@ -22,85 +19,113 @@ try:
 except IndexError:
     pass
 
-
 import carla
-#from queue import Queue
-from queue import Empty
-from PIL import Image
-import numpy as np
-from RL_funcs import *
-import Carla_funcs as cf
-import gcg
 
 
 # %%
-# RL variables
+# SETUP RL VARIABLE
+
+# K-shooting time horizon
 H = 16
+
+# Number of K-shoots
 K = 5
+
+# Steering actions as a percentage of ~70° 
 action_space = np.array([-0.3, 0, 0.3])
+
+# Vehicle speed in m/s (this is decomposed into an orthogonal cartesian coordinate system and only approximates horizontal velocities)
 vehicle_speed = 5
+
+# Simulator time steps
 delta_t = 0.1
 
 
 # %%
-# Initialize Network
+# INITIALIZE THE TF NETWORK MODEL
+
+# Use these two lines to build a TF model from scratch
 model = gcg.computation_graph(H)
 print('GCG Built!')
+
+# Use these two lines to load a previously built TF model (used for outages and troubleshooting)
 # model = gcg.load_old_model('../models/model.tf')
 # print('GCG Loaded!')
 
 
 # %%
-#  Carla Setup
-# image_queue = Queue()
-# collision_queue = Queue()
+#  CARLA SIMULATOR SETUP
 
+# Initialize a connection with the CARLA server and setup all CARLA related experimental parameters and supports
 client, world, vehicle, camera, collision, orig_settings, image_queue, collision_queue = cf.setup(time_step = delta_t, img_x = 128, img_y = 72, speed=vehicle_speed)
     
 
 # %%
+# ITERATE THROUGH A PREDIFIEND SET OF EPISODES
 
-# Begin loop
-big_loop_counter = 4000
+# Define the number of episodes
+#big_loop_counter = 4000
+big_loop_counter = 2
+
+# Target a collision free runtime of 10 minutes
 step_max = int((10 * 60) / delta_t)
+
+# Setup an object to hold the stacked image state
 img_stack = None
 
+# Setup some metrics
 cum_steps = 0
 cum_steps_per_ep = []
 steps_per_ep = []
 
+# Iterate through the episodes
 for i in range(big_loop_counter):
-    # Initialize datasets
+    
+    # Initialize and episodic dataset
     dataset_I = []
     dataset_a = []
     y_labels = []
     y_buffer = np.zeros(H)
 
-    # Sim loop
     episode_done = False
-    # get an initial image
     
+    # Retrieve data from the CARLA simulator (resulting from a "tick", see Carla_funcs.py)
     world.get_snapshot().frame
+    
+    # Retrieve and image from the image queue.  
     img = image_queue.get(True, 1.0)
     
+    # Navigate through the simulator (up to 10 minutes collision free)
     for step in range(step_max):
-    #for step in range(9):
+    
+        # Setup objects for a single iteration
         img_stack = cf.preprocess_img(img, img_stack)
         rwd_list = []
         action_sets = []
     
+        # Do K-shooting
         for k in range(K):
+            
+            # Initialize a new action vector
             action_input = generate_actions(action_space, H)
+            
+            # Predict the probabilities of collision
             y_hats = gcg.run(model, img_stack, action_input)
+            
+            # Calculate the reward
             rwd = reward_function(y_hats)
             rwd_list.append(rwd)
             action_sets.append(action_input)
     
+        # Extract the highest reward and action from the K-shoot
         best_action_set = best_actions(action_sets, rwd_list)
+        
+        # Take a single action in CARLA Simulator and observe the new state and "reward" signal 
         img, collided = cf.take_action(world, vehicle, image_queue, img, collision_queue, best_action_set[0])
         dataset_I.append(img_stack)
         dataset_a.append(best_action_set)
         
+        # Populate actual collision labels
         if collided == 1:
             if step+1 >= H:
 
@@ -121,9 +146,10 @@ for i in range(big_loop_counter):
     cum_steps_per_ep.append(cum_steps)
     steps_per_ep.append(step)
 
-
+    # Update/train the model at the end of every episode
     gcg.train(model, dataset_I, dataset_a, y_labels)
     
+    # Outputs for observing training, saving the model and storing metrics
     if (i % 10 == 0) and (i > 0):
         print('Big Loop iteration - ', i)
     
@@ -156,15 +182,16 @@ for i in range(big_loop_counter):
         plt.title('Figure 2')
         plt.savefig('../images/figure2.png')
 
-    
-
-
+    # Clean-up the CARLA server and get it read for the next episode
     cf.close(world, camera, collision, vehicle, orig_settings)
     vehicle, camera, collision, image_queue, collision_queue = cf.spawn_car(world= world, img_x = 128, img_y = 72, speed=vehicle_speed)
 
+
+
 # %%
-# End and exit
-# world.apply_settings(orig_settings)
+# SHUT DOWN CARLA
+
+# Leave the CARLA Server in a clean state
 cf.close(world, camera, collision, vehicle, orig_settings)
 
 
